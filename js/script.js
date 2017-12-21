62970d50216e9856fbe9d68b82278b83f1c05186
Leslie55bg <lesliebodingarcia@gmail.com>
2017-12-21 14:57:17 +0100

    modif

---
+++ js/script.js
@@ -0,0 +1,24 @@
+var player = 0
+var player_1 = 1
+var player_2 = 2
+
+// onclick="document.location='lien.html'
+//var bouton = document.getElementById("button");
+
+
+function play(case){ //fonction  play   +  onclick html case
+
+  if  // si c'est le joueur 1 qui a cliqué
+// alors on ajoute la classe pour placer le signe
+
+//c'est au tour du joueur 2
+// sinon si c'est le joueur 2 qui a cliqué
+// alors on ajoute la classe pour placer le signe
+
+// c'est au tour du joueur 1
+
+// afficher dans le html quel joueur doit jouer
+
+// fermeture de la fonction
+
+}
